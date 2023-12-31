923ae6ab8149d41915f56fa6dc91ed4350fd0845
Himanshu Choudhary <hchoudhary525@gmail.com>
2023-12-31 13:45:47 +0530

    git

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,10 +1,16 @@
-print("Hello World")
-5+5
+# Importing main libraries mm
+
+import matplotlib
+matplotlib.use('TkAgg')
 import numpy as np
 import pandas as pd
-import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib_inline
+import seaborn as sns
 
-print("hello world")
-print(10+10)
-va="creating a variable"
-print(va)
+# Importing data from system, we have our data in CSV form
+df=pd.read_csv("C:/Users/hchou/OneDrive/Desktop/Advertising.csv")
+df.head()   # Checking head of the data
+
+
+
